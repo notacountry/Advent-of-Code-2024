62505ccba92c3e440c18c07ea763e9d94b54d51a
zl4624 <zl4624@ic.ac.uk>
2024-12-08 22:52:35 +0000

    Q1 P2

--- 1/advent.hs
+++ 1/advent.hs
@@ -1,18 +1,29 @@
 import Data.Char
 import Data.List
+import Data.Maybe
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
     let (ns, ns') = unzip $ map lineToInt $ lines input
-    print(distance (sort ns, sort ns'))
+    print(((sum .) . zipWith ((abs .) . (-))) (sort ns) (sort ns'))
+    print(similarity ns (createMap ns'))
     pure ()
 
 lineToInt :: String -> (Int, Int)
-lineToInt cs = (read n :: Int, read n' :: Int)
-    where
-      (n, rest) = span (isNumber) cs
-      n' = snd $ break (isNumber) rest
+lineToInt cs = let [l, r] = map (read :: String -> Int) (words cs) in (l, r)
 
-distance :: ([Int], [Int]) -> Int
-distance = uncurry ((sum .) . zipWith ((abs .) . (-)))
+createMap :: [Int] -> [(Int, Int)]
+createMap (x:xs) = case (findIndex (\n -> x == fst n) oldMap) of
+    Nothing -> (x, 1) : oldMap
+    Just i  ->
+        let (first, (_, n):last) = splitAt i oldMap
+        in first ++ (x, n + 1) : last
+  where oldMap = createMap xs
+createMap _ = []
+
+similarity :: [Int] -> [(Int, Int)] -> Int
+similarity (x:xs) map = case (lookup x map) of
+    Nothing -> similarity xs map
+    Just n  -> x * n + similarity xs map
+similarity _ map = 0
